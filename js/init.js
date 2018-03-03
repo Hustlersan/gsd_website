003d12d5ee5f8ac51f0ee913166abce0b24b4811
hustlersan <nos.megid@gmail.com>
2018-03-04 00:34:21 +0200

    added main banner [master]

--- js/init.js
+++ js/init.js
@@ -3,6 +3,18 @@
 
     $('.button-collapse').sideNav();
 
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[id=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
